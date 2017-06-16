dd7dc6536b4c38d587a17363274ce9698a133531
Kimberly Harrod <kim@fios.com>
2017-06-16 13:48:59 -0500

    Comment model

---
+++ models/Comment.js
@@ -0,0 +1,20 @@
+// Require mongoose
+var mongoose = require("mongoose");
+
+//Create a schema class
+var Schema = mongoose.Schema;
+
+//Create the Comment schema
+var CommentSchema = new Schema({
+	title: {
+		type: String
+	},
+	body: {
+		type: String
+	}
+});
+
+//Create the Comment model with the CommentSchema
+var Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
