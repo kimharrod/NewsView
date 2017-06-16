7f7556b69ed95fc6a905b2370e34334b26e9ae03
Kimberly Harrod <kim@fios.com>
2017-06-16 13:38:24 -0500

    Article model

---
+++ models/Article.js
@@ -0,0 +1,39 @@
+// Require mongoose
+var mongoose = require("mongoose");
+// Create Schema class
+var Schema = mongoose.Schema;
+
+// Create article schema
+var ArticleSchema = new Schema({
+
+	title: {
+		type: String;
+		required: true
+	},
+	image: {
+		type: String
+	},
+	link: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	saved: {
+		type: Boolean,
+		default: false
+	},
+	//This only saves one comment's ObjectID. Ref refers to the Comment model.
+	comments: [{
+		type: Schema.Types.ObjectId,
+		ref: "Comment"
+	}]
+});
+
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model("Article", ArticleSchema);
+
+//Export the model
+module.exports = Article;
