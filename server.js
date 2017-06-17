4554a2e0656773ebf6602e18ad481dbe8ffa78b4
Kimberly Harrod <kim@fios.com>
2017-06-16 22:08:59 -0500

    Route to scrape website

--- server.js
+++ server.js
@@ -39,4 +39,67 @@ db.once("open", function() {
 	console.log("Mongoose connection successful.");
 });
 
+// Routes
+// ======
+
+// A GET request to scrape the progressive.org website
+app.get("/scrape", function(req, res) {
+	// First get the body of the html with request
+	request("http://progressive.org/dispatches", function(error, response, html) {
+		// Then load that into cheerio and save it to $ for a shorthand selector
+		var $ = cheerio.load(html);
+		// Now grab every p within the mp-classiclist, and do the following:
+		$(".mp-classiclist p.mp-description").each(function(i, element) {
+
+			// Save an empty result object
+			var result = {};
+
+			// Add the title, link, image and description of every article
+			// Save them as properties of the result object
+			result.title = $(this).children("a").attr("title");
+			result.link = $(this).children("a").attr("href");
+			result.image = $($(this).parent()).parent().find(".mp-lazyload").attr("src");
+			// Remove the "more" at the end of the description
+			var str = $(this).text();
+			result.description = str.slice(0, -4);
+
+			console.log("\nImage URL: " + result.image);
+
+			console.log(JSON.stringify(result));
+
+			// Check to see if an article with title already exists in the database
+			Article.count({ title: result.title }, function (err, count) {
+
+				// If it isn't a duplicate, then:
+				if (count < 1) {
+
+					// Using our Article model, create a new entry
+					// This effectively passes the result object to the entry
+					// (including the title, image, description and link)
+					var entry = new Article(result);
+
+					// Save entry to the db
+					entry.save(function(err, doc) {
+
+						// Log any errors
+						if (err) {
+							console.log(err);
+						}
+
+						// Or log the doc
+						else {
+							console.log(doc);
+						}
+					});
+
+				} //end if count < 1
+			}); // end Article.count	
+
+		});
+	});
+
+	// Tell the browser that we finished scraping the text
+	res.send("Scrape Complete");
+});
+
 
